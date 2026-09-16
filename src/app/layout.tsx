06e0a41ed50dbf0f119f0867d67b3caf5b0e386f
swakraft <hugo.beets2003@gmail.com>
2026-09-16 15:07:49 +0200

    added script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ export default function RootLayout({
       <head>
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script src="/config.js" />
+        <script defer src="https://umami.chauffagistes-btc.fr/script.js" data-website-id="09f3ce4a-aa69-4353-839b-577f8432954e"></script>
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png"></link>
